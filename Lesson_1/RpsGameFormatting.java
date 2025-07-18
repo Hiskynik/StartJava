9e253c11afcd6ef5213034960c7fae70e1ce49d8
nikita <nikita.moscow2011@gmail.com>
2025-07-18 15:44:18 +0300

    Исправлены недостатки форматирования в классе RpsGameFormatting

--- Lesson_1/RpsGameFormatting.java
+++ Lesson_1/RpsGameFormatting.java
@@ -8,11 +8,10 @@ public class RpsGameFormatting {
         String paper = "P";
 
         // Ход первого игрока
-        String name1  = "HEL";
+        String name1 = "HEL";
         Random r = new Random();
         int position1 = r.nextInt(1, 100);
         String sign1 = rock;
-
 
         if (position1 > 66) {
             sign1 = paper;
@@ -20,14 +19,14 @@ public class RpsGameFormatting {
             sign1 = scissors;
         }
 
-        System.out.println("Ход "+ name1 + ": ");
+        System.out.println("Ход " + name1 + ":");
 
         for (int i = 0; i < 5; i++) {
-        System.out.print (rock + "\r") ;
+            System.out.print(rock + "\r");
             Thread.sleep(100);
-        System.out.print (scissors + "\r" );
+            System.out.print(scissors + "\r");
             Thread.sleep(100);
-        System.out.print (paper + "\r");
+            System.out.print(paper + "\r");
             Thread.sleep(100);
         }
         System.out.println(sign1);
@@ -43,27 +42,26 @@ public class RpsGameFormatting {
             sign2 = scissors;
         }
 
-        System.out.println("Ход " + name2 + ": ");
+        System.out.println("Ход " + name2 + ":");
 
-        for (int i = 0 ; i < 5 ; i ++) {
+        for (int i = 0; i < 5; i++) {
             System.out.print(rock + "\r");
             Thread.sleep(100);
-            System.out.print( scissors + "\r" );
+            System.out.print(scissors + "\r");
             Thread.sleep(100);
-            System.out.print ( paper + "\r");
+            System.out.print(paper + "\r");
             Thread.sleep(100);
         }
-        System.out.println (sign2);
+        System.out.println(sign2);
 
         if (sign1.equals(sign2)) {
             System.out.println("Победила дружба!");
             return;
         }
 
-
-        boolean isEqualName1 = sign1.equals(rock) && sign2.equals(scissors) ||
-                sign1.equals(scissors) && sign2.equals(paper) ||
-                sign1.equals(paper) && sign2.equals(rock);
+        boolean isEqualName1 = sign1.equals(rock) && sign2.equals(scissors)
+                || sign1.equals(scissors) && sign2.equals(paper)
+                || sign1.equals(paper) && sign2.equals(rock);
 
         if (isEqualName1) {
             System.out.println("\nПобедил - " + name1);
