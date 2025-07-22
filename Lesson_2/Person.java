925df558b1e2825936b882d2bf7d56e55386844a
nikita <nikita.moscow2011@gmail.com>
2025-07-22 15:36:32 +0300

    Добавлен класс Person в директорию Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "Male";
+    String name = "John";
+    float height = 1.85f; 
+    float weight = 85.5f; 
+    int age = 35;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    void sit() {
+        System.out.println("siting");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void talk() {
+        System.out.println("talking");
+    }
+
+    void learnJava() {
+        System.out.println("learning Java");
+    }
+}
