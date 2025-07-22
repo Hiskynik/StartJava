e5626a004991f4b7aff143dd19df6ade765a743e
nikita <nikita.moscow2011@gmail.com>
2025-07-22 17:36:18 +0300

    Добавлены классы Wolf и WolfTest в директорию Lesson_2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,26 @@
+public class Wolf {
+    String nickname;
+    String gender;
+    String color;
+    int age;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    void sit() {
+        System.out.println("siting");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void howl() {
+        System.out.println("howling");
+    }
+
+    void hunt() {
+        System.out.println("hunting");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest {
+    public static void main(String[] args) {
+        Wolf wolfOne = new Wolf();
+
+        wolfOne.nickname = "Bobik";
+        wolfOne.gender = "male";
+        wolfOne.color = "grey";
+        wolfOne.age = 7;
+
+        System.out.println("nickname = " + wolfOne.nickname);
+        System.out.println("male = " + wolfOne.gender);
+        System.out.println("grey = " + wolfOne.color);
+        System.out.println("age = " + wolfOne.age);
+
+        wolfOne.walk();
+        wolfOne.sit();
+        wolfOne.run();
+        wolfOne.howl();
+        wolfOne.hunt();
+    }
+}
