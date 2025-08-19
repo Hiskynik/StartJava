ca57abe976362d60fab359b31c48542cc4da3b3a
nikita <nikita.moscow2011@gmail.com>
2025-08-20 01:05:31 +0300

    Изменена структура проекта

--- src/com/startjava/lesson_1/base/VariableNamesTheme.java
+++ src/com/startjava/lesson_1/base/VariableNamesTheme.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_1.base;
+
 import java.time.LocalDate;
 import java.time.Month;
 
--- src/com/startjava/lesson_2_3/method/naming/MethodNameHelper.java
+++ src/com/startjava/lesson_2_3/method/naming/MethodNameHelper.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.method.naming;
+
 class MethodNameHelper {
     public static String getCurrent() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
