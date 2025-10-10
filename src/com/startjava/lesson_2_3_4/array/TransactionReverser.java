f39a6e39fe9624d555d9547f177443dc962aabb4
nikita <nikita.moscow2011@gmail.com>
2025-10-10 18:13:06 +0300

    Заменен for на for-each в классе TransactionReverser

--- src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
+++ src/com/startjava/lesson_2_3_4/array/TransactionReverser.java
@@ -4,40 +4,39 @@ import java.util.Arrays;
 
 class TransactionReverser {
     public static void main(String[] args) {
-        processAndDisplay(new int[0]);
-        System.out.println();
+        run(new int[0]);
 
-        processAndDisplay(null);
-        System.out.println();
+        run(null);
 
-        processAndDisplay(new int[]{5});
-        System.out.println();
+        run(new int[]{5});
 
-        processAndDisplay(new int[]{6, 8, 9, 1});
-        System.out.println();
+        run(new int[]{6, 8, 9, 1});
 
-        processAndDisplay(new int[]{13, 8, 5, 3, 2, 1, 1});
+        run(new int[]{13, 8, 5, 3, 2, 1, 1});
     }
 
-    private static void processAndDisplay(int[] original) {
+    private static void run(int[] original) {
         int[] reversed = reverse(original);
         displayTransactions(original, reversed);
+        System.out.println();
     }
 
     private static int[] reverse(int[] transactions) {
         if (transactions == null || transactions.length == 0) {
-            return transactions; // Просто возвращаем как есть
+            return transactions;
         }
 
         int[] reversed = new int[transactions.length];
-        for (int i = 0; i < transactions.length; i++) {
-            reversed[i] = transactions[transactions.length - 1 - i];
+        int i = 0;
+        for (int transaction : transactions) {
+            reversed[transactions.length - 1 - i] = transaction;
+            i++;
         }
 
         return reversed;
     }
 
-    public static void displayTransactions(int[] original, int[] reversed) {
+    private static void displayTransactions(int[] original, int[] reversed) {
         System.out.println("Исходные транзакции: " + Arrays.toString(original));
         System.out.println(" В обратном порядке: " + Arrays.toString(reversed));
     }
