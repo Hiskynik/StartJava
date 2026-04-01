e46727a880d4cb78a13a811b6294299c9b8da90f
nikita <nikita.moscow2011@gmail.com>
2026-04-01 22:06:32 +0300

    Рефакторинг HangmanGameMain и HangmanGame

--- src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
@@ -1,16 +1,12 @@
 package com.startjava.lesson_2_3_4.hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class HangmanGame {
-    private final String secretWord;
-    private final boolean[] guessed;
-    private int mistakeParts;
-    private final boolean[] lettersUsed;
-    private final StringBuilder wrongLetters;
-    private final Scanner scanner;
-
-    private static final String[] HANGMAN_STAGES = {
+    private static final String[] WORDS = {"ява", "питон", "котлин", "свифт", "бейсик", "си"};
+    private static final Random RANDOM = new Random();
+    private static final String[] GALLOWS = {
             "_______",
             "|     |",
             "|     @",
@@ -18,109 +14,90 @@ public class HangmanGame {
             "|    / \\",
             "| GAME OVER!"
     };
-    private static final int MAX_PARTS = HANGMAN_STAGES.length - 1;
+    private static final int MAX_PARTS = GALLOWS.length - 1;
+
+    private final String secretWord;
+    private final boolean[] guessed;
+    private int mistakeParts;
+    private final boolean[] lettersUsed;
+    private final StringBuilder wrongLetters;
+    private final Scanner scanner;
+    private final StringBuilder displayMask;
 
     public HangmanGame(String secretWord, Scanner scanner) {
         this.secretWord = secretWord.toLowerCase();
         this.guessed = new boolean[this.secretWord.length()];
         this.mistakeParts = 0;
-        this.lettersUsed = new boolean[65536];
+        this.lettersUsed = new boolean[33];
         this.wrongLetters = new StringBuilder();
         this.scanner = scanner;
+        this.displayMask = new StringBuilder("*".repeat(this.secretWord.length()));
+    }
+
+    public static String getRandomWord() {
+        return WORDS[RANDOM.nextInt(WORDS.length)];
     }
 
     public void start() {
-        System.out.println("Добро пожаловать в игру Виселица!");
-        System.out.println("Загадано слово из " + secretWord.length() + " букв.");
-        System.out.println("У вас есть " + MAX_PARTS + " частей виселицы. " +
-                "Каждая правильная буква убирает одну часть, неправильная - добавляет.");
-        System.out.println("Чтобы выиграть, откройте все буквы до того, " +
-                "как виселица будет полностью нарисована.\n");
-
+        printWelcome();
         while (!isGameOver()) {
-            System.out.println(getHangmanPicture());
-            System.out.println("Слово: " + getDisplayWord());
-            System.out.println("Ошибочные буквы: " + getWrongLetters());
-            System.out.println("Осталось попыток: " + getRemainingAttempts());
+            printGameState();
             System.out.print("Введите букву: ");
-
             String input = scanner.nextLine().trim();
-
             if (input.length() != 1) {
                 System.out.println("Пожалуйста, введите одну букву.");
                 continue;
             }
-
             char letter = input.charAt(0);
-            int result = guess(letter);
-
-            switch (result) {
-                case 1:
-                    System.out.println("Буква '" + letter + "' есть в слове! Часть виселицы убрана.");
+            boolean validMove = guess(letter);
+            if (validMove) {
+                if (isWin()) {
+                    printWin();
                     break;
-                case 0:
-                    System.out.println("Буква '" + letter + "' отсутствует. Часть виселицы добавлена.");
+                } else if (mistakeParts >= MAX_PARTS) {
+                    printLoss();
                     break;
-                case 2:
-                    System.out.println("Буква '" + letter + "' уже была введена " +
-                            "и отсутствует в слове. Попробуйте другую.");
-                    continue;
-                case 3:
-                    System.out.println("Буква '" + letter + "' уже была угадана. Попробуйте другую.");
-                    continue;
-                case -1:
-                    System.out.println("Введите русскую букву.");
-                    continue;
-                default:
-                    System.out.println("Некорректный ввод.");
-                    continue;
-            }
-
-            if (isWin()) {
-                System.out.println("\nПоздравляем! Вы угадали слово: " + secretWord);
-                break;
-            } else if (mistakeParts >= MAX_PARTS) {
-                System.out.println("\nВы проиграли! Виселица полностью нарисована.");
-                System.out.println("Загаданное слово было: " + secretWord);
-                break;
+                }
             }
         }
     }
 
-    private boolean isRussianLetter(char c) {
-        c = Character.toLowerCase(c);
-        return (c >= 'а' && c <= 'я') || c == 'ё';
-    }
-
-    public int guess(char letter) {
-        if (!isRussianLetter(letter)) {
-            return -1;
+    public boolean guess(char letter) {
+        int idx = getRussianLetterIndex(letter);
+        if (idx == -1) {
+            System.out.println("Введите русскую букву.");
+            return false;
         }
         letter = Character.toLowerCase(letter);
-        int idx = letter;
-
         if (lettersUsed[idx]) {
-            return secretWord.indexOf(letter) != -1 ? 3 : 2;
+            if (secretWord.indexOf(letter) != -1) {
+                System.out.println("Буква '" + letter + "' уже была угадана. Попробуйте другую.");
+            } else {
+                System.out.println("Буква '" + letter + "' уже была введена " +
+                        "и отсутствует в слове. Попробуйте другую.");
+            }
+            return false;
         }
-
         lettersUsed[idx] = true;
-
         if (secretWord.indexOf(letter) != -1) {
             for (int i = 0; i < secretWord.length(); i++) {
                 if (secretWord.charAt(i) == letter) {
                     guessed[i] = true;
+                    displayMask.setCharAt(i, Character.toUpperCase(letter));
                 }
             }
             if (mistakeParts > 0) {
                 mistakeParts--;
             }
-            return 1;
+            System.out.println("Буква '" + letter + "' есть в слове! Часть виселицы убрана.");
+            return true;
         } else {
             if (mistakeParts < MAX_PARTS) {
                 mistakeParts++;
                 wrongLetters.append(letter).append(' ');
             }
-            return 0;
+            System.out.println("Буква '" + letter + "' отсутствует. Часть виселицы добавлена.");
+            return true;
         }
     }
 
@@ -136,15 +113,7 @@ public class HangmanGame {
     }
 
     public String getDisplayWord() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (guessed[i]) {
-                sb.append(Character.toUpperCase(secretWord.charAt(i)));
-            } else {
-                sb.append('*');
-            }
-        }
-        return sb.toString();
+        return displayMask.toString();
     }
 
     public String getWrongLetters() {
@@ -157,16 +126,51 @@ public class HangmanGame {
 
     public String getHangmanPicture() {
         int linesToShow = mistakeParts + 1;
-        if (linesToShow > HANGMAN_STAGES.length) {
-            linesToShow = HANGMAN_STAGES.length;
+        if (linesToShow > GALLOWS.length) {
+            linesToShow = GALLOWS.length;
         }
         StringBuilder picture = new StringBuilder();
         for (int i = 0; i < linesToShow; i++) {
-            picture.append(HANGMAN_STAGES[i]);
+            picture.append(GALLOWS[i]);
             if (i < linesToShow - 1) {
                 picture.append("\n");
             }
         }
         return picture.toString();
+    }
+
+    private int getRussianLetterIndex(char c) {
+        c = Character.toLowerCase(c);
+        if (c == 'ё') return 6;
+        if (c >= 'а' && c <= 'я') {
+            if (c > 'е') return c - 'а' - 1;
+            else return c - 'а';
+        }
+        return -1;
+    }
+
+    private void printWelcome() {
+        System.out.println("Добро пожаловать в игру Виселица!");
+        System.out.println("Загадано слово из " + secretWord.length() + " букв.");
+        System.out.println("У вас есть " + MAX_PARTS + " частей виселицы. " +
+                "Каждая правильная буква убирает одну часть, неправильная - добавляет.");
+        System.out.println("Чтобы выиграть, откройте все буквы до того, " +
+                "как виселица будет полностью нарисована.\n");
+    }
+
+    private void printGameState() {
+        System.out.println(getHangmanPicture());
+        System.out.println("Слово: " + getDisplayWord());
+        System.out.println("Ошибочные буквы: " + getWrongLetters());
+        System.out.println("Осталось попыток: " + getRemainingAttempts());
+    }
+
+    private void printWin() {
+        System.out.println("\nПоздравляем! Вы угадали слово: " + secretWord);
+    }
+
+    private void printLoss() {
+        System.out.println("\nВы проиграли! Виселица полностью нарисована.");
+        System.out.println("Загаданное слово было: " + secretWord);
     }
 }
--- src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
@@ -1,34 +1,22 @@
 package com.startjava.lesson_2_3_4.hangman;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class HangmanGameMain {
-    private static final String[] WORDS = {"ява", "питон", "котлин", "свифт", "бейсик", "си"};
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        boolean playAgain;
 
         do {
-            String secretWord = WORDS[random.nextInt(WORDS.length)];
+            String secretWord = HangmanGame.getRandomWord();
             HangmanGame game = new HangmanGame(secretWord, scanner);
-
             game.start();
 
             System.out.print("\nХотите сыграть еще? (yes/no): ");
-            String answer;
-            while (true) {
-                answer = scanner.nextLine().trim().toLowerCase();
-                if (answer.equals("yes") || answer.equals("no")) {
-                    break;
-                } else {
-                    System.out.print("Введите корректный ответ [yes / no]: ");
-                }
+            String answer = scanner.nextLine().trim().toLowerCase();
+            if (answer.equals("no")) {
+                break;
             }
-            playAgain = answer.equals("yes");
-        } while (playAgain);
+        } while (true);
 
         System.out.println("Спасибо за игру! До свидания.");
         scanner.close();
